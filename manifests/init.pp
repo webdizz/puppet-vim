1ed2b0c20b578797e6f01d43773ec24db03a4d57
Johan Haals <johan.haals@gmail.com>
2013-03-11 12:12:36 +0100

    Merged setup.pp into init.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,10 +9,31 @@
 # There's an optional home_dir parameter if you use a custom home directory.
 
 class vim {
-  require vim::setup
+
   package { 'vim':
     require => Package['mercurial']
   }
   # Install mercurial since the vim brew package don't satisfy the requirement
   package { 'mercurial': }
+
+  file { ["/Users/${::boxen_user}/.vim",
+    "/Users/${::boxen_user}/.vim/autoload",
+    "/Users/${::boxen_user}/.vim/bundle"]:
+    ensure  => directory,
+    recurse => true,
+  }
+
+  repository { "/Users/${::boxen_user}/.vim/vim-pathogen":
+    source => 'tpope/vim-pathogen'
+  }
+
+  file { "/Users/${::boxen_user}/.vim/autoload/pathogen.vim":
+    target  => "/Users/${::boxen_user}/.vim/vim-pathogen/autoload/pathogen.vim",
+    require => [
+      File["/Users/${::boxen_user}/.vim"],
+      File["/Users/${::boxen_user}/.vim/autoload"],
+      File["/Users/${::boxen_user}/.vim/bundle"],
+      Repository["/Users/${::boxen_user}/.vim/vim-pathogen"]
+    ]
+  }
 }
