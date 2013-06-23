5a9660b38f7ebf61a1153984fc4d124d9a41903b
Johan Haals <johan.haals@gmail.com>
2013-06-23 23:00:18 +0200

    satisfy docutils requirement

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,8 +16,16 @@ class vim {
   package { 'vim':
     require => Package['mercurial']
   }
-  # Install mercurial since the vim brew package don't satisfy the requirement
-  package { 'mercurial': }
+  # Install mercurial since the vim brew package don't satisfy the requirement (vim is fetched using $ hg)
+  package { 'mercurial':
+    require => Package['docutils']
+  }
+  # docutils is required by mercurial.
+  # https://github.com/boxen/puppet-vim/issues/12
+  package { 'docutils':
+    ensure   => installed,
+    provider => pip,
+  }
 
   file { [$vimdir,
     "${vimdir}/autoload",
--- spec/classes/vim_spec.rb
+++ spec/classes/vim_spec.rb
@@ -8,7 +8,14 @@ describe 'vim' do
     should contain_package('vim').with({
       'require' => 'Package[mercurial]'
     })
-    should contain_package('mercurial')
+    should contain_package('mercurial').with({
+      'require' => 'Package[docutils]'
+    })
+    should contain_package('docutils').with({
+      'ensure'  => 'installed',
+      'provider' => 'pip',
+    })
+
     should contain_repository("/Users/jhaals/.vim/vim-pathogen")
     should contain_file("/Users/jhaals/.vim/autoload").with({
       'ensure'    => 'directory',
