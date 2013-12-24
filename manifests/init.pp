a9f4997ab96a57f2a31b504ff68c6da871e513ab
Marco Valtas <mavcunha@gmail.com>
2013-12-24 11:13:58 -0200

    Simplifying things, moved bundle to pathogen

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,3 @@
 class vim($vimdir = "/Users/${::boxen_user}/.vim") {
-
-#  include vim::pathogen
-
   package { 'vim': }
-
-  file { [$vimdir,
-    "${vimdir}/bundle"]:
-    ensure  => directory,
-    recurse => true,
-  }
-
 }
--- manifests/pathogen.pp
+++ manifests/pathogen.pp
@@ -1,6 +1,21 @@
-class vim:pathogen {
+class vim::pathogen {
+
   repository { "${vim::vimdir}/bundle/vim-pathogen":
     source => 'tpope/vim-pathogen',
     require => File[$vim::vimdir]
   }
+
+  file { [$vim::vimdir, "${vim::vimdir}/bundle"]:
+    ensure  => directory,
+    recurse => true,
+  }
+
+  define bundle() {
+    $bundle = split($name, '/')
+    repository { "${vim::vimdir}/bundle/${bundle[1]}":
+      source  => $name,
+      require => File["${vim::vimdir}/bundle"],
+    }
+  }
+
 }
